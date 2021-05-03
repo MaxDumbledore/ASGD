6a84e850684f99a19fa4acb0b9c07a5b70096919
404610159@qq.com <404610159@qq.com>
2021-05-03 15:38:24 +0800

    w

--- Session.cpp
+++ Session.cpp
@@ -3,15 +3,23 @@
 //
 
 #include "Session.h"
+#include <iostream>
 #include "SessionManager.h"
 #include "Utils.h"
-#include <iostream>
 
 using namespace std;
 
-Session::Session(int clientID, asio::ssl::stream<asio::ip::tcp::socket> socket, SessionManager &manager) :
-        clientID(clientID), socket(std::move(socket)), manager(manager) {
+Session::Session(int clientId,
+                 asio::ssl::stream<asio::ip::tcp::socket> socket,
+                 SessionManager& manager)
+    : clientId(clientId), socket(std::move(socket)), manager(manager) {}
 
+void Session::myDebug(const std::string& func, const asio::error_code& err) {
+    std::clog << "With Client " << clientId << ": " << func;
+    if (err)
+        std::clog << " failed because " << err.message() << std::endl;
+    else
+        std::clog << " succeeded!" << std::endl;
 }
 
 void Session::start() {
@@ -19,29 +27,45 @@ void Session::start() {
 }
 
 void Session::handshake() {
+    string func = __func__;
     auto self(shared_from_this());
-    socket.async_handshake(asio::ssl::stream_base::server,
-                           [this, self](const asio::error_code &error) {
-                               if (!error) {
-                                   sendIdAndParams();
-                                   std::clog << "handshake succeeded!" << std::endl;
-                               } else
-                                   std::cerr << error.message() << std::endl;
-                           });
+    socket.async_handshake(
+        asio::ssl::stream_base::server,
+        [this, self, func = std::move(func)](const asio::error_code& err) {
+            myDebug(func, err);
+            if (!err)
+                sendIdAndInitialParams();
+        });
 }
 
 void Session::stop() {
     socket.shutdown();
 }
 
-void Session::sendIdAndParams() {
+void Session::sendIdAndInitialParams() {
+    string func = __func__;
     auto self(shared_from_this());
-    buf = intToByte(clientId);
-    buf.reserve(buf.size() + manager.params().length() * 4);
-    for(auto f:manager.params().getParams())
-
+    buf += intToBytes(clientId) + floatVecToStream(manager.params().getData());
     asio::async_write(socket, asio::buffer(buf),
-                      [this, self](const asio::error_code &err, std::size_t) {
-
+                      [this, self, func = std::move(func)](
+                          const asio::error_code& err, std::size_t) {
+                          myDebug(func, err);
+                          if (!err)
+                              receiveUpdate();
                       });
+}
+
+void Session::receiveUpdate() {
+    string func = __func__;
+    auto self(shared_from_this());
+    buf.resize(manager.params().size() * 4);
+    asio::async_read(socket, asio::buffer(buf),
+                     [this, self, func = std::move(func)](
+                         const asio::error_code& err, std::size_t) {
+                         myDebug(func, err);
+                         if (err)
+                             return;
+                         manager.params().update(
+                             streamToFloatVec(buf, manager.params().size()));
+                     });
 }
